1b1e4fcaebcc41c4bb87d463811a12ee1cd0a089
doruk.tekel <doruktekel55@gmail.com>
2025-03-24 18:21:47 +0300

    added cronvs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import job from "./lib/cron.js";
 
 import { connectDb } from "./lib/db.js";
 import authRouter from "./routes/authRoutes.js";
@@ -9,9 +10,9 @@ import bookRouter from "./routes/bookRoutes.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+job.start();
 app.use(express.json());
 app.use(cors());
-
 app.use("/api/auth", authRouter);
 app.use("/api/book", bookRouter);
 
